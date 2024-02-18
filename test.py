ac3a22323a5f45bb978bac45c3f0634e66cdf699
Austin.chen <dspring1995@gmail.com>
2024-02-18 11:49:17 +0800

    2024-02-18 1st commit

--- test.py
+++ test.py
@@ -1,15 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+url = "https://www.ithome.com.tw/"
 
-r = requests.get("https://yahoo.com.tw/")
+r = requests.get(url)
 
 try:
     if r.status_code == 200:
         soup = BeautifulSoup(r.text, "lxml")
-        print(soup)
-
+        news = soup.find_all("div", class_ = "item span4")
+        print(len(news))
+        for new in news:
+            print(new.find("p", class_ = "title").find("a").text)
+            print(url + new.find("p", class_ = "title").find("a").get("href"))
+            print(new.find("p", class_= "post-at").text)
 except Exception as e:
     print(e)
     print("那ㄟ安內~?")
-else:
-    print("Successful!!")
+# else:
+#     print("Successful!!")
