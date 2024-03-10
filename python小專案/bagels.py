5d3f29537c7d93481e60691bdf245e5398e00aa2
Austin.chen <dspring1995@gmail.com>
2024-03-10 21:03:54 +0800

    進度上傳~

--- python小專案/bagels.py
+++ python小專案/bagels.py
@@ -1,3 +1,10 @@
+"""Bagels, by Al Sweigart al@inventwithpython.com
+A deductive logic game where you must guess a number based on clues.
+This code is available at https://nostarch.com/big-book-small-python-programming
+A version of this game is featured in the book, "Invent Your Own
+Computer Games with Python" https://nostarch.com/inventwithpython
+Tags: short, game, puzzle"""
+
 import random
 
 NUM_DIGITS = 3
@@ -25,5 +32,21 @@ clues would be Fermi Pico.'''.format(NUM_DIGITS))
         numGuesses = 1
         while numGuesses <= MAX_GUESSES:
             guess = ""
-            while
-        
+            while len(guess) != NUM_DIGITS or not guess.isdecimal():
+                print("Guess #{}: ".format(numGuesses))
+                guess = input("> ")
+
+            clues = getClues(guess, secretNum)
+            print(clues)
+            numGuess += 1
+
+            if guess == secretNum:
+                break
+            if numGuesses > MAX_GUESSES:
+                print("You ran out of guesses.")
+                print("The answer was {}.".format(secretNum))
+
+        print("Do you want to play again? (yes or no)")
+        if not input("> ").lower().startswith("y"):
+            break
+    print("Thanks for playing!")
