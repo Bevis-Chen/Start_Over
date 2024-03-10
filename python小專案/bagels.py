464da8e8ae61e63ff87a1c64c94ddf40312e7cda
Austin.chen <dspring1995@gmail.com>
2024-03-10 18:49:18 +0800

    先存個檔~~

--- python小專案/bagels.py
+++ python小專案/bagels.py
@@ -1,1 +1,29 @@
 import random
+
+NUM_DIGITS = 3
+MAX_GUESSES = 10
+
+def main():
+    print('''Bagels, a deductive logic game.
+By Al Sweigart al@inventwithpython.com
+
+I am thinking of a {}-digit number with no repeated digits.
+Try to guess what it is. Here are some clues:
+When I say:    That means:
+  Pico         One digit is correct but in the wrong position.
+  Fermi        One digit is correct and in the right position.
+  Bagels       No digit is correct.
+
+For example, if the secret number was 248 and your guess was 843, the
+clues would be Fermi Pico.'''.format(NUM_DIGITS))
+    
+    while True:
+        secretNum = getSecretNum()
+        print("I have thought up a number.")
+        print(' You have {} guesses to get it.'.format(MAX_GUESSES))
+
+        numGuesses = 1
+        while numGuesses <= MAX_GUESSES:
+            guess = ""
+            while
+        
