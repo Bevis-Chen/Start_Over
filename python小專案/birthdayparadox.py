5abfdbbeb1a545991ff7bfedd4276287663618ca
Austin.chen <dspring1995@gmail.com>
2024-03-19 23:55:54 +0800

    birthdayparadox.py打字中~~

--- python小專案/birthdayparadox.py
+++ python小專案/birthdayparadox.py
@@ -9,3 +9,33 @@ def getBirthdays(numberOfBirthdays):
         birthday = startOfYear + randomNumberOfDays
         birthdays.append(birthday)
     return birthdays
+
+def getMatch(birthdays):
+    if len(birthdays) == len(set(birthdays)):
+        return None
+    
+    for a, birthdayA in enumerate(birthdays):
+        for b, birthdayB in enumerate(birthdays[a + 1 :]):
+            if birthdayA == birthdayB:
+                return birthdayA
+            
+print('''Birthday Paradox, by Al Sweigart al@inventwithpython.com
+
+The birthday paradox shows us that in a group of N people, the odds
+that two of them have matching birthdays is surprisingly large.
+This program does a Monte Carlo simulation (that is, repeated random
+simulations) to explore this concept.
+
+(It's not actually a paradox, it's just a surprising result.)
+''')
+
+MONTHS = ('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+          'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec')
+
+while True:
+    print("How many birthdays shall I generate? (Max 100)")
+    response = input("> ")
+    if response.isdecimal() and (0 < int(response )<= 100):
+        numBDays = int(response)
+        break
+print()
