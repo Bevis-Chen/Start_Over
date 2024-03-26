976e69a968441646826d4f15f7b905156bf62c63
Austin.chen <dspring1995@gmail.com>
2024-03-27 07:31:50 +0800

    存個...檔?

--- 爬蟲/PTT.py
+++ 爬蟲/PTT.py
@@ -2,21 +2,21 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-# url = r"https://www.google.com/search?q=android&sca_esv=100a8d72724cfbcd&ei=mvT-Zf6jC77d1e8P68Gq2A4&ved=0ahUKEwj-2Nb-2IqFAxW-bvUHHeugCusQ4dUDCBA&oq=andorid&gs_lp=Egxnd3Mtd2l6LXNlcnAiB2FuZG9yaWQyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEdIwglQAFgAcAB4ApABAJgBAKABAKoBALgBDMgBAJgCAaACBZgDAOIDBRIBMSBAiAYBkAYKkgcBMaAHAA&sclient=gws-wiz-serp"
-# url = r"https://www.google.com/search"
-
 url = r"https://www.google.com/search?q=android"
+
 header = {"user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
+data = {
+    "from" : "/bbs/Gossiping/index.html",
+    "yes" : "yes"}
 
-# resp = requests.get(url, headers = header, params = {"q" : "android"})
 session = requests.Session()
-resp = session.get(url, headers = header)
+resp = session.post(url, headers = header, data = data)
 
-if resp.status_code == 200:
-    # print(resp.text)
-    soup = BeautifulSoup(resp.text, "html.parser")
-    datas = soup.select("div.yuRUbf")
-    for i in datas:
-        print(i.select_one("h3").text)
-        print(i.select_one("a").get("href"))
-        print("*" * 60)
+# if resp.status_code == 200:
+print(resp.text)
+    # soup = BeautifulSoup(resp.text, "html.parser")
+    # datas = soup.select("div.yuRUbf")
+    # for i in datas:
+    #     print(i.select_one("h3").text)
+    #     print(i.select_one("a").get("href"))
+    #     print("*" * 60)
--- 爬蟲/googleSearch.py
+++ 爬蟲/googleSearch.py
@@ -2,13 +2,10 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-# url = r"https://www.google.com/search?q=android&sca_esv=100a8d72724cfbcd&ei=mvT-Zf6jC77d1e8P68Gq2A4&ved=0ahUKEwj-2Nb-2IqFAxW-bvUHHeugCusQ4dUDCBA&oq=andorid&gs_lp=Egxnd3Mtd2l6LXNlcnAiB2FuZG9yaWQyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEcyBBAAGEdIwglQAFgAcAB4ApABAJgBAKABAKoBALgBDMgBAJgCAaACBZgDAOIDBRIBMSBAiAYBkAYKkgcBMaAHAA&sclient=gws-wiz-serp"
-# url = r"https://www.google.com/search"
-
 url = r"https://www.google.com/search?q=android"
+
 header = {"user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
 
-# resp = requests.get(url, headers = header, params = {"q" : "android"})
 resp = requests.get(url, headers = header)
 
 if resp.status_code == 200:
