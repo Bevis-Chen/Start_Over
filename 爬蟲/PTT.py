015152520da10306b941fd79c0feebdbec62d0fb
Austin.chen <dspring1995@gmail.com>
2024-03-28 01:52:21 +0800

    勉強完成一頁~

--- 爬蟲/PTT.py
+++ 爬蟲/PTT.py
@@ -2,19 +2,24 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-url = r"https://www.google.com/search?q=android"
+url = r"https://www.ptt.cc/ask/over18"
 
 header = {"user-agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
 data = {
-    "from" : "/bbs/Gossiping/index.html",
+    "from" : "/bbs/Gossiping/index39171.html",
     "yes" : "yes"}
 
 session = requests.Session()
 resp = session.post(url, headers = header, data = data)
 
-# if resp.status_code == 200:
-print(resp.text)
-    # soup = BeautifulSoup(resp.text, "html.parser")
+if resp.status_code == 200:
+    # print(resp.text)
+    soup = BeautifulSoup(resp.text, "html.parser")
+    datas = soup.select("div.r-ent")
+    # print(datas)
+    for data in datas:
+        print(data.select_one("a").text)
+        print("https://www.ptt.cc" + data.select_one("a").get("href"))
     # datas = soup.select("div.yuRUbf")
     # for i in datas:
     #     print(i.select_one("h3").text)
