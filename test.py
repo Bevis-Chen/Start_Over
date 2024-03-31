235a24d47dc968e461ed5ea55f2e241fe7c71143
Austin.chen <dspring1995@gmail.com>
2024-03-31 23:42:40 +0800

    今日....不戰....

--- test.py
+++ test.py
@@ -9,45 +9,48 @@ data = { "from" : "/bbs/Gossiping/index.html",
           "yes" : "yes" }
 
 session = requests.Session()
-resp = session.post(url, headers = header, data = data)
-if resp.status_code == 200:
-    # print(resp.text)
-    soup = BeautifulSoup(resp.text, "html.parser")
-    btn = soup.select("div.btn-group>a.btn")
-    for i, b in enumerate(btn) : 
-        page_max = 0
-        if i == 3:
-            url_get = b.get("href")
-            re_page = r"\d"
-            page_max_str = re.findall(re_page, url_get)
-            page_max = eval("".join(page_max_str))
-            break
-    # print(url_get)
-    print()
-# try:
-    # for p in range(39319, 39322):
-    # pages = "/bbs/Gossiping/index{}.html".format(p)
-    # data = {
-    #     "from" : pages,
-    #     "yes" : "yes"}
-    # session = requests.Session()
-    # resp = session.post(url, headers = header, data = data)
-    # time.sleep(2)
-    # if resp.status_code == 200:
-            # print(resp.text)
-            # soup = BeautifulSoup(resp.text, "html.parser")
-            # datas = soup.select("div.r-ent")
-            # print(datas)
-            # for data in datas:
-            #     if data.select_one("title").text != None:
-            #         if "Re: " not in data.select_one("title a").text:
-            #             print(data.select_one("title a").text)
-            #             print("https://www.ptt.cc" + data.select_one("a").get("href"))                        
-            # datas = soup.select("div.yuRUbf")
-            # for i in datas:
-            #     print(i.select_one("h3").text)
-            #     print(i.select_one("a").get("href"))
-            #     print("*" * 60)
-# except Exception as e:
-#     print(e, "哪尼~~~?")
-    # print(data)
+try:
+    resp = session.post(url, headers = header, data = data)
+    if resp.status_code == 200:
+        # print(resp.text)
+        soup = BeautifulSoup(resp.text, "html.parser")
+        btn = soup.select("div.btn-group>a.btn")
+        for i, b in enumerate(btn) : 
+            page_max = 0
+            if i == 3:
+                url_get = b.get("href")
+                re_page = r"\d"
+                page_max_str = re.findall(re_page, url_get)
+                page_max = eval("".join(page_max_str))
+                break
+        # print(url_get)
+        # print(page_max)
+    for p in range(1,10):
+        pages = "/bbs/Gossiping/index{}.html".format(p)
+        data = {
+            "from" : pages,
+            "yes" : "yes"}
+        session = requests.Session()
+        resp = session.post(url, headers = header, data = data)
+        time.sleep(2)
+        if resp.status_code == 200:
+                print(resp.text)
+                soup = BeautifulSoup(resp.text, "html.parser")
+                datas = soup.select("div.r-ent")
+                print(datas)
+                for a in datas:
+                    if a.select_one("title").text != None:
+                        if "Re: " not in a.select_one("title a").text:
+                            print(a.select_one("title a").text)
+                            print("https://www.ptt.cc" + a.select_one("a").get("href"))                        
+except Exception as e:
+    print(e, "哪尼~~~?")
+    print(data)
+
+'''
+datas = soup.select("div.yuRUbf")
+for i in datas:
+    print(i.select_one("h3").text)
+    print(i.select_one("a").get("href"))
+    print("*" * 60)
+'''
