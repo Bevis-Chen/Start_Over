6085cd46ec6460091e2a404ac1f1076e6706099b
Austin.chen <dspring1995@gmail.com>
2024-04-22 21:34:30 +0800

    奇怪哩~? + MI300X終於跑起來了~

--- test.py
+++ test.py
@@ -2,4 +2,5 @@ from datetime import datetime
 
 current_datetime = datetime.now()
 
+print(current_datetime.day - 1)
 print(str(current_datetime.month) + "/" + str(current_datetime.day))
--- 爬蟲/PTT/ptt_by_teacher.py
+++ 爬蟲/PTT/ptt_by_teacher.py
@@ -7,32 +7,29 @@ def pttNews():
     Date = str(current_datetime.month) + "/" + str(current_datetime.day)
 
     url = r"https://www.ptt.cc/ask/over18"
-
     header = {"user-agent" : 
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
     data = { "from" : "/bbs/Gossiping/index.html",
             "yes" : "yes" }
-
+    
     session = requests.Session()
     session.post(url, headers = header, data = data)
     url2 = "https://www.ptt.cc/bbs/Gossiping/index.html"
-    # print(Date)
-    # print(type(Date))
+
     while True:
         r = session.post(url2, data = data, headers = header)
         temp = r.text.split('<div class="r-list-sep"></div>')[0]
         soup = BeautifulSoup(temp, "html.parser")
         titles = soup.select("div.r-ent div.title a")
         dates = soup.select("div.r-ent div.meta div.date")
+        # if dates[0].text != Date:
+        #     return         
         url2 = "https://www.ptt.cc" + soup.select("div.btn-group.btn-group-paging a")[1].get("href")
 
         for title, time in zip(titles, dates):
-            if time.text != Date:
-                return 
             if "Re:" not in title.text:
                 print(title.text)
                 print("https://www.ptt.cc" + title.get("href"))
                 print(time.text)
                 
-
 pttNews()
