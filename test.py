a42f835e53ead8dc1d39e953e04d7fcec1fd355b
Austin.chen <dspring1995@gmail.com>
2024-04-28 18:38:39 +0800

    解決一個內容爬取~~

--- test.py
+++ test.py
@@ -2,17 +2,21 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 
-try_url = "https://www.ptt.cc/bbs/Gossiping/M.1714235956.A.1E2.html"
-
 header = {"user-agent" : 
 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"}
-data = { "from" : "/bbs/Gossiping/M.1714235956.A.1E2.html",
+data = { "from" : "/bbs/Gossiping/index.html",
         "yes" : "yes" }
 
+url = r"https://www.ptt.cc/ask/over18"
+
 session = requests.Session()
-r = session.get(try_url, headers = header)
-# url2 = "https://www.ptt.cc/bbs/Gossiping/index.html"
+session.post(url, headers = header, data = data)
+url2 = "https://www.ptt.cc/bbs/Gossiping/index.html"
+try_url = "https://www.ptt.cc/bbs/Gossiping/M.1714235956.A.1E2.html"
+r = session.post(try_url, data = data, headers = header)
 
 soup = BeautifulSoup(r.text, "html.parser")
-# for text in soup.select_one("div.article-meta-tag"):
-print(soup.find("div", class_ = "article-meta-tag"))
+text1 = soup.find("div", id = "main-content").text
+text2 = text1.split("--")[0].split("\n")[1:]
+content = "\n".join(text2)
+print(content)
